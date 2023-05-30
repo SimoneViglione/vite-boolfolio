852f19a4224a92b1755788429da5bb67a6d89038
Simone Viglione <zimoviglio51@gmail.com>
2023-05-30 18:42:59 +0200

    milestone 5 completata

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,7 +2,16 @@
     <div class="container ms_container">
         <div class="row">
             <div class="col-4" v-for="project in projects">
-                
+                <div class="card">
+                    <img v-if="project.cover_image" :src="`${this.baseUrl}/storage/${project.cover_image}`" class="card-img-top">
+                    <img v-else src="https://cdn.icon-icons.com/icons2/1462/PNG/512/120nophoto_100007.png" class="card-img-top"/>
+                    <div class="card-body">
+                        <h5 class="card-title">{{project.title}}</h5>
+                        <h5>{{project.type?.name}}</h5>
+    
+                        <a href="#" class="btn btn-primary">Vai al Progetto </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div> 
@@ -26,7 +35,7 @@
 
                 .then(response => {
                     console.log(response);
-                    this.posts = response.data.results;
+                    this.projects = response.data.results;
                 });
             }
         },
